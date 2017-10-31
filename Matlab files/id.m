965d644ca79f1f731c4be1f99d8ff08ae6d907b5
Hanna Johansson <hanna93.hej@gmail.com>
2017-10-31 13:57:01 +0100

    added skeleton of comments

--- Matlab files/id.m
+++ Matlab files/id.m
@@ -1,12 +1,63 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % function id = tnm034(im) 
 %
-% im: Image of unknown face, RGB-image in uint8format inthe
+% im: Image of unknown face, RGB-image in uint8format in the
 % range [0,255]
 %
 % id: The identity number (integer) of the identified person,
-% i.e.‘1’, ‘2’,...,‘16’for the persons belonging to ‘db1’ 
-% and ‘0’for allother faces.
+% i.e.‘1’, ‘2’,...,‘16’ for the persons belonging to ‘db1’ 
+% and ‘0’ for allother faces.
 %
 % Your program code.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+disp('-----------------------------------------------');
+disp('  PART 1: Face detection ');
+disp('-----------------------------------------------');
+% PART 1: Face detection
+
+
+
+
+disp('-----------------------------------------------');
+disp('  PART 2: Face alignment ');
+disp('-----------------------------------------------');
+% PART 2: Face alignment
+
+
+
+
+disp('-----------------------------------------------');
+disp('  PART 3: Appearence Normalization ');
+disp('-----------------------------------------------');
+% PART 3: Appearance Normalization
+
+
+
+
+disp('-----------------------------------------------');
+disp('  PART 4: Feature Description ');
+disp('-----------------------------------------------');
+% PART 4: Description
+
+
+
+
+disp('-----------------------------------------------');
+disp('  PART 5: Feature Extraction ');
+disp('-----------------------------------------------');
+% PART 5: Feature Extraction
+
+
+
+
+
+disp('-----------------------------------------------');
+disp('  PART 6: Matching ');
+disp('-----------------------------------------------');
+% PART 6: Matching
+
+
+
+
+
