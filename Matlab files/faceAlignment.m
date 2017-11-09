c1ae5e764533c29f38560c97e0403b073329b967
Madeleine Rapp <madra484@student.liu.se>
2017-11-09 09:18:07 +0100

    läst in db1 och laggt till facealignment

--- Matlab files/faceAlignment.m
+++ Matlab files/faceAlignment.m
@@ -7,8 +7,26 @@
 
 function faceAlignment()
 
-% Eyemap C 1/3 *(Cb^ + (Cr inv))^2 + Cb/Cr)
+load ('db0Images');
+image = db0Images{1};
 
+% EyemapC = 1/3 *(Cb^2 + (Cr inv))^2 + Cb/Cr)
+iycbcr=rgb2ycbcr(image); %Convert to colorspace YCbCr
+iycbcr = im2double(iycbcr); % Normalize
+imshow(iycbcr);
+
+% Split into separate chanels
+y=iycbcr(:,:,1); 
+cb=iycbcr(:,:,2);
+cr=iycbcr(:,:,3);
+
+% Create components for equation
+cb2=cb.^2;
+crinv2=(1-cr).^2;
+cbcr=cb./cr;
+
+eyeMap = (cb2 + crinv2 + cbcr) ./3; 
+imshow(eyeMap);
 
 
 end
