f69cbc3d5136e862267728836ebbcefaf27de99a
Madeleine Rapp <madra484@student.liu.se>
2017-11-09 10:25:22 +0100

    faceAlignment

--- Matlab files/faceAlignment.m
+++ Matlab files/faceAlignment.m
@@ -8,9 +8,11 @@
 function faceAlignment()
 
 load ('db0Images');
-image = db0Images{1};
+load ('db1Images');
 
-% EyemapC = 1/3 *(Cb^2 + (Cr inv))^2 + Cb/Cr)
+image = db1Images{2};
+
+% ---- EyeMapC = 1/3 *(Cb^2 + (Cr inv))^2 + Cb/Cr)  ----
 iycbcr=rgb2ycbcr(image); %Convert to colorspace YCbCr
 iycbcr = im2double(iycbcr); % Normalize
 imshow(iycbcr);
@@ -25,8 +27,40 @@ cb2=cb.^2;
 crinv2=(1-cr).^2;
 cbcr=cb./cr;
 
-eyeMap = (cb2 + crinv2 + cbcr) ./3; 
-imshow(eyeMap);
+eyeMapC = (cb2 + crinv2 + cbcr) ./3; 
 
+
+%  ---- EyeMapL = Y(x,y) * gsigma((x,y) / Y(x,y) ** gsigma((x,y) + 1 ----
+% * - dilation ** - erotion
+imgGray = rgb2gray(image); % Change to graymap
+imgGrayHist = histeq(imgGray);
+
+%{
+subplot(2,2,1);
+imshow(imgGray);
+title('imgGray');
+
+subplot(2,2,2);
+imshow(imgGrayHist);
+title('imgGrayHist');
+%}
+
+SE = strel('disk', 15, 8); % radius = 15, n(number of segments) = 8
+
+% Create components for equation
+numerator =  imdilate(imgGrayHist, SE); % T?ljare
+denumerator = 1 + imerode(imgGrayHist, SE); % N?mnare
+
+eyeMapL = numerator ./denumerator; 
+
+%{
+subplot(2,2,3);
+imshow(eyeMapC);
+title('eyeMapC');
+
+subplot(2,2,4);
+imshow(eyeMapL);
+title('EyeMapL');
+%}
 
 end
