0a2a5e06320728d09187625f2d20bc7f4d8f46ba
Hanna Johansson <hanjo306@student.liu.se>
2017-11-27 09:33:04 +0100

    now merge solved

--- Matlab files/faceAlignment.m
+++ Matlab files/faceAlignment.m
@@ -11,7 +11,7 @@ load ('db0Images');
 load ('db1Images');
 load ('db1Faces');
 
-currentFace = 12;
+currentFace = 11;
 image = db1Images{currentFace};
 faceMask = db1Faces{currentFace};
 %image = db0Images{2};
@@ -20,7 +20,7 @@ faceMask = db1Faces{currentFace};
 iycbcr = rgb2ycbcr(image); 
 
 %----------------------------------------------------------------
-%                    faceMask
+%                 faceMask
 %----------------------------------------------------------------
 % Make faceMask binary
 faceMaskBin = im2bw(faceMask, 0.01);    %use imbinarize instead of im2bw
@@ -29,58 +29,14 @@ SDil = strel('disk', 6, 8); % radius = 10, n(number of segments) = 8
 faceMask = imerode(imdilate(faceMaskBin, SDil), SEr);
 
 %----------------------------------------------------------------
-%                   eyes and mouth detection
+%                 eyes and mouth detection
 %----------------------------------------------------------------
-
 finalEyeMap = eyeMap(image, iycbcr, faceMask);
 finalMouthMap = mouthMap(image, iycbcr, faceMask);
 
-
 %----------------------------------------------------------------
-%                    Plot images, use uint8 to plot images
+%                 plot images, use uint8 to plot images
 %----------------------------------------------------------------
-% Change to graymap
-imgGray = rgb2gray(image); 
-imgGrayHist = histeq(imgGray);
-
-% Create components for equation
-SE = strel('disk', 15, 8); % radius = 15, n(number of segments) = 8
-numerator =  imdilate(imgGrayHist, SE); % T?ljare
-denumerator = 1 + imerode(imgGrayHist, SE); % N?mnare
-
-% Create eyeMapL and normalize
-eyeMapL = numerator ./ denumerator;
-eyeMapL = normalize_matrix(eyeMapL,0,255);
-
-
-%----------------------------------------------------------------
-%                    Combine eyeMapC and eyeMapL
-%----------------------------------------------------------------
-% Multiply C and L 
-imgMult = (eyeMapC .* eyeMapL);
-
-%----------------------------------------------------------------
-%                    Refine eyeMap to get finalEyeMap
-%----------------------------------------------------------------
-
-% Normalize imgMult
-imgMult = normalize_matrix(imgMult,0,1);
-
-% Dilate imgMult
-imgMultDil =  imdilate(imgMult, SDil);
-
-% Make imgMultDil binary
-imgMultDilBin = im2bw(imgMultDil, 0.5);    %use imbinarize instead of im2bw
-
-
-%{
-figure; 
-subplot(2,2,1);
-imshow(eyeMapCBinary);
-title('eyeMapCBinary'); 
-
-
-% -------------- PLOT ---------------
 figure;
 subplot(2,2,1);
 imshow(image);
@@ -92,24 +48,11 @@ title('faceMask');
 
 subplot(2,2,3);
 imshow(finalEyeMap);
-
-title('finalEyeMap'); 
-%}
-
-%               --------------- Mouth detection ---------------
-% MouthMap = Cr^2 * (Cr^2 - eta*Cr/Cb)^2
-% eta = 0,95 * 1/n * (sum of all elements in Cr^2 ) /(sum of all elements in Cr/Cb ) 
-
-% Split into separate chanels
-y =histeq(iycbcr(:,:,1)); 
-cb=histeq(iycbcr(:,:,2));
-cr=(iycbcr(:,:,3));
-
-
+title('finalEyeMap');
+ 
 subplot(2,2,4);
 imshow(finalMouthMap);
-title('finalMouthMap');
-
+title('finalMouthMap'); 
 
 %----------------------------------------------------------------
 %                    Find position of objects
