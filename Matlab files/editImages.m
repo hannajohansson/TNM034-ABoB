7e0637e15090f3d02efa6936850f8d84e1e57bee
Madeleine Rapp <madra484@student.liu.se>
2017-11-28 13:48:23 +0100

    removed for loop

--- Matlab files/editImages.m
+++ Matlab files/editImages.m
@@ -1,5 +1,34 @@
-function editImages()
+function editedImage = editImages(image)
 
+%               -------------- image --------------
+    originalImage = im2double(image);
+    % figure;
+    % subplot(1, 3, 1), imshow(originalImage), title('Original image');
+
+    % Step 2: Convert to gray so we can get the mean luminance.
+    grayImage = rgb2gray(originalImage);
+
+    % Step 3: Extract the individual red, green, and blue color channels.
+    redChannel = originalImage(:, :, 1);
+    greenChannel = originalImage(:, :, 2);
+    blueChannel = originalImage(:, :, 3);
+
+    % Step 4: Mean 
+    meanRed = mean2(redChannel);
+    meanGreen = mean2(greenChannel);
+    meanBlue = mean2(blueChannel); 
+    meanGray = mean2(grayImage);
+
+    % Step 5: Make all channels have the same mean
+    redChannel = (redChannel * meanGray / meanRed);
+    greenChannel = (greenChannel * meanGray / meanGreen);
+    blueChannel = (blueChannel * meanGray / meanBlue);
+
+    % Step 6: Recombine separate color channels into a single, true color RGB image.
+    editedImage = cat(3, redChannel, greenChannel, blueChannel);
+    editedImage = min(editedImage, 1);
+
+%{
 %               -------------- db0 --------------
     % Step 1: Load images
     load ('db0Images');
@@ -77,5 +106,7 @@ function editImages()
     
     %save cell of images
     save 'db1Images' db1Images;  
+  
+    %}
 
 end
