c8d61fb79c1357fa9d2c41a966fd43226e0f121e
Sandra Pettersson <sandrap.pettersson@gmail.com>
2017-12-08 10:10:58 +0100

    New test images for tone value

--- Matlab files/tnm034.m
+++ Matlab files/tnm034.m
@@ -1,20 +1,22 @@
 function [id] = tnm034(im)
 
-    % im: Image of unknown face, RGB-image in uint8format in the range [0,255]
+    % im: Image of unknown face, RGB-image in uint8 format in the range [0,255]
     % id: The identity number (integer) of the identified person
     
     % RUN "processDB" ONE TIME BEFORE THIS FUNCTION
-
-    % REMOVE LATER
-    im = imread('testIm/db1_09_combination.jpg');
-    %im = imread('testIm/db1_09_rotation_+5.jpg');
-    %im = imread('images/db0/db0_3.jpg');
-    %im = imread('images/db2/bl_01.jpg');
-    
-    % im = im*1.3; %+30%
-    % im = im*0.7; %-30%
     
     close all;
+
+    % REMOVE LATER
+    %im = imread('testIm/db1_09_combination.jpg');
+    %im = imread('testIm/db1_09_rotation_+5.jpg');
+    im = imread('images/db1/db1_09.jpg');
+    %im = imread('images/db2/bl_01.jpg');
+    
+    %im = im*1.3; %+30%
+    im = im*0.7; %-30%
+    
+    figure, imshow(im);
 
     % Run the face detection for the image im to be compared with the db
     disp('--- Processing the input image.. ---');
